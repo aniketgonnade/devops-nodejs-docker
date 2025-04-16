6ae69f60f73c4d1c10349d096f6cd96217d266af
root <root@ip-172-31-15-163.ap-south-1.compute.internal>
2025-04-16 09:44:57 +0000

    made changes to app.js 1

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('🚀 DevOps App Runnin once again!');
+  res.send('🚀 DevOps App Runnin once agai aniket!');
 });
 
 app.listen(port, () => {
